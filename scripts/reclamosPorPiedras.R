503f0c5c6d03043d4820d310bc0f27b3768a21b6
Matias Lespiau <mlespiau@avature.net>
2014-05-12 09:11:48 -0300

    Add new plot

--- scripts/reclamosPorPiedras.R
+++ scripts/reclamosPorPiedras.R
@@ -1,5 +1,6 @@
 library(sqldf)
 library(lattice)
+
 reclamos <- read.csv("./data/ausa-reclamos.csv", header=TRUE, sep=";")
 reclamos$fecha <- as.Date(reclamos$fechareclamo, format="%Y-%m-%d %H:%M:%S")
 reclamos$anio <- format(reclamos$fecha, format="%Y")
@@ -10,8 +11,13 @@ reclamos <- sqldf("SELECT * FROM reclamos WHERE anio != '1988'")
 if (!file.exists("plots")) {
   dir.create("plots");
 }
+
 png("plots/reclamosPorPiedras.png", height=680, width=850, bg="transparent")
 hist(as.integer(reclamos[reclamos$motivo == "PIEDRAS", c("anio")]), labels=TRUE, right=FALSE, main="Cantidad de reclamos anuales por piedras en las autopistas de Buenos Aires", xlab="Año", ylab="Cantidad de reclamos", col="blue")
+dev.off()
+
+png("plots/reclamosPorAnio.png", height=680, width=850, bg="transparent")
+hist(as.integer(sqldf("SELECT anio from reclamos where clase = 'RECLAMOS'")[,1]), labels=TRUE, right=FALSE, main="Cantidad de reclamos anuales en las autopistas de Buenos Aires", xlab="Año", ylab="Cantidad de reclamos", col="blue")
 dev.off()
 
 reclamosPorMesAnio <- sqldf("SELECT anio, mes, COUNT(*) as cnt FROM reclamos WHERE motivo = 'PIEDRAS' GROUP BY anio, mes ORDER BY anio DESC, mes DESC")
